2b7389c2655335d6626105c0718b78882d08a76e
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-11-28 10:56:58 -0300

    feat: add a new column (product_quantity: INTEGER) to the relation between orders and products

--- server/src/database/schema/order_products.ts
+++ server/src/database/schema/order_products.ts
@@ -8,4 +8,5 @@ export const orderProducts = table("order_products", {
   id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
   orderId: t.uuid("order_id").references(() => orders.id).notNull(),
   productId: t.uuid("product_id").references(() => products.id).notNull(),
+  productQuantity: t.integer("product_quantity").default(1).notNull(),
 });
