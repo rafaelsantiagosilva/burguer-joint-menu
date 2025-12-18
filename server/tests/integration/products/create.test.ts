e772863a2cd893f6a32397ab566d23ff44988a79
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-18 20:27:10 -0300

    test: create product service integration test

---
+++ server/tests/integration/products/create.test.ts
@@ -0,0 +1,58 @@
+import { InvalidProductPriceError } from "@/errors/InvalidProductPriceError.ts";
+import { DrizzleProductsRepository } from "@/repositories/drizzle/drizzle-products-repository.ts";
+import { CreateProductService } from "@/services/products/create.ts";
+import { resetDatabase } from "@/tests/setup/db.ts";
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+
+describe("Create Product Service (Unit)", () => {
+  let productsRepository: DrizzleProductsRepository;
+  let sut: CreateProductService;
+
+  beforeEach(async () => {
+    await resetDatabase();
+
+    productsRepository = new DrizzleProductsRepository();
+    sut = new CreateProductService(productsRepository);
+  });
+
+  afterAll(async () => {
+    await resetDatabase();
+  });
+
+  it("should be able to create a product", async () => {
+    const product = await sut.execute({
+      name: "Product 1",
+      price: 10,
+      description: "Description 1",
+      imagePath: null
+    });
+
+    const createdProduct = await productsRepository.findById(product.id);
+
+    expect(createdProduct!.name).toBe("Product 1");
+    expect(createdProduct!.price).toBe(10);
+    expect(createdProduct!.description).toBe("Description 1");
+  });
+
+  it("should not be able to create a product with a negative price", async () => {
+    await expect(async () =>
+      await sut.execute({
+        name: "Product 1",
+        price: -10,
+        description: "Description 1",
+        imagePath: null
+      })
+    ).rejects.toBeInstanceOf(InvalidProductPriceError);
+  });
+
+  it("should not be able to create a product with a zero price", async () => {
+    await expect(async () =>
+      await sut.execute({
+        name: "Product 1",
+        price: 0,
+        description: "Description 1",
+        imagePath: null
+      })
+    ).rejects.toBeInstanceOf(InvalidProductPriceError);
+  });
+});
