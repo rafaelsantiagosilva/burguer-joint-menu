3ab4bd569c0c4da2eb9c8ec960b281e94adffc23
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-18 20:28:12 -0300

    test: delete product service integration test

---
+++ server/tests/integration/products/delete.test.ts
@@ -0,0 +1,29 @@
+import { ResourceNotFoundError } from "@/errors/ResourceNotFoundError.ts";
+import { DrizzleProductsRepository } from "@/repositories/drizzle/drizzle-products-repository.ts";
+import { DeleteProductService } from "@/services/products/delete.ts";
+import { beforeEach, describe, expect, it } from "vitest";
+
+describe("Delete Product Service (Unit)", () => {
+  let productsRepository: DrizzleProductsRepository;
+  let sut: DeleteProductService;
+
+  beforeEach(() => {
+    productsRepository = new DrizzleProductsRepository();
+    sut = new DeleteProductService(productsRepository);
+  });
+
+  it("should be able to delete an product", async () => {
+    const { id } = await productsRepository.create("Product 1", null, 10, null);
+
+    await sut.execute({ id });
+    const product = await productsRepository.findById(id);
+
+    expect(product).toBeNull();
+  });
+
+  it("should not be able to delete an product that does not exist", async () => {
+    await expect(async () =>
+      await sut.execute({ id: "product-1" })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
