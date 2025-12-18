17ebeac99bf2148a927c5df8713369031430a685
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-18 20:32:35 -0300

    test: update user service integration test

---
+++ server/tests/integration/products/update.test.ts
@@ -0,0 +1,61 @@
+import { ResourceNotFoundError } from "@/errors/ResourceNotFoundError.ts";
+import { DrizzleProductsRepository } from "@/repositories/drizzle/drizzle-products-repository.ts";
+import { UpdateProductService } from "@/services/products/update.ts";
+import { resetDatabase } from "@/tests/setup/db.ts";
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+
+describe("Update Product Service (Integration)", () => {
+  let productsRepository: DrizzleProductsRepository;
+  let sut: UpdateProductService;
+
+  beforeEach(async () => {
+    await resetDatabase();
+
+    productsRepository = new DrizzleProductsRepository();
+    sut = new UpdateProductService(productsRepository);
+  });
+
+  afterAll(async () => {
+    await resetDatabase();
+  });
+
+  it("should be able to update a product", async () => {
+    const product = await productsRepository.create(
+      "Product 1",
+      null,
+      10,
+      null
+    );
+
+    await sut.execute({
+      ...product,
+      name: "Updated Product",
+      price: 20,
+      isAvaliable: false
+    });
+
+    const updatedProduct = await productsRepository.findById(product.id);
+
+    expect(updatedProduct).toEqual(expect.objectContaining({
+      id: product.id,
+      name: "Updated Product",
+      price: 20,
+      isAvaliable: false,
+      imagePath: null,
+      description: null
+    }));
+  });
+
+  it("should not be able to update a non-existing product", async () => {
+    await expect(async () =>
+      await sut.execute({
+        id: "non-existing-product",
+        name: "Updated Product",
+        price: 20,
+        isAvaliable: false,
+        imagePath: null,
+        description: null
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
