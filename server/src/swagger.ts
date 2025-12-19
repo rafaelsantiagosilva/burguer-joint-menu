bf5f2532a9fd118ca0c922be4108e4d0b67dff2f
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-19 11:58:55 -0300

    docs: creating swagger components

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -22,10 +22,78 @@ const swaggerOptions = {
           scheme: "bearer",
           bearerFormat: "JWT",
         },
+      },
+      schemas: {
+        GetUserProfileResponse: {
+          type: "object",
+          properties: {
+            name: { type: "string", nullable: true },
+            phone: { type: "string" },
+            isAdmin: { type: "boolean" },
+            address: { type: "string", nullable: true },
+          },
+        },
+        InvalidProductPriceResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "One or more products in the order have an invalid price." },
+          }
+        },
+        InvalidProductQuantityResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "One or more products in the order have an invalid quantity." },
+          }
+        },
+        NotAvailableProductResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "One or more products in the order are not available." },
+          }
+        },
+        WithoutProductsResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "An order must have at least one product." },
+          }
+        },
+        UnauthorizedResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "Unauthorized" },
+          }
+        },
+        InvalidCredentialsResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "Invalid email or password." },
+          }
+        },
+        NotFoundResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "Resource not found" },
+          }
+        },
+        UserAlreadyExistsResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "User with this email already exists." },
+          }
+        },
+        InternalServerErrorResponse: {
+          type: "object",
+          properties: {
+            message: { type: "string", example: "Internal server error" },
+          }
+        }
       }
     }
   },
-  apis: ["./src/routes/*.ts", "./src/controllers/*.ts", "./src/app.ts"],
+  apis: [
+    "./src/api/routers/*.ts",
+    "./src/app.ts"
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
