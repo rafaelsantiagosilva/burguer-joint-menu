f75be547f5ac9dad81c2deea1a993cd952862a6b
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-20 16:16:16 -0300

    fix: fixing the error message; changing from "email" to "phone"

--- server/src/errors/UserAlreadyExistsError.ts
+++ server/src/errors/UserAlreadyExistsError.ts
@@ -3,6 +3,6 @@ import { AppError } from "./AppError.ts";
 
 export class UserAlreadyExistsError extends AppError {
   constructor() {
-    super("User with this email already exists.", StatusCodes.CONFLICT);
+    super("User with this phone already exists.", StatusCodes.CONFLICT);
   }
 }
