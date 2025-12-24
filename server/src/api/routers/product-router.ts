f0155e71eccf481ce2cda1df7fb0cdd93f44b16c
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-23 22:19:13 -0300

    feat: adding two missing routes - list and update - to the product router

--- server/src/api/routers/product-router.ts
+++ server/src/api/routers/product-router.ts
@@ -15,5 +15,7 @@ export class ProductRouter {
     this.routes.post("/create", auth, adminVerify, this.productsController.create.bind(this.productsController));
     this.routes.delete("/delete/:id", auth, adminVerify, this.productsController.delete.bind(this.productsController));
     this.routes.patch("/disable/:id", auth, adminVerify, this.productsController.disable.bind(this.productsController));
+    this.routes.get("/list", auth, this.productsController.list.bind(this.productsController));
+    this.routes.put("/update/:id", auth, adminVerify, this.productsController.update.bind(this.productsController));
   }
 }
