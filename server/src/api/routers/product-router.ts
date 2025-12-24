7ab1c8e078ba50377f8e6962740ad667e1760f4f
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-23 22:18:12 -0300

    feat: products router

---
+++ server/src/api/routers/product-router.ts
@@ -0,0 +1,19 @@
+import type IProductsRepository from "@/repositories/IProductsRepository.ts";
+import { Router } from "express";
+import { ProductsController } from "../controllers/products-controller.ts";
+import { adminVerify } from "../middlewares/admin-verify.ts";
+import { auth } from "../middlewares/auth.ts";
+
+export class ProductRouter {
+  public routes: Router = Router();
+  private productsController: ProductsController | null = null;
+
+  constructor(private productsRepository: IProductsRepository) {
+    this.productsController = new ProductsController(this.productsRepository);
+
+    this.routes.patch("/active/:id", auth, adminVerify, this.productsController.active.bind(this.productsController));
+    this.routes.post("/create", auth, adminVerify, this.productsController.create.bind(this.productsController));
+    this.routes.delete("/delete/:id", auth, adminVerify, this.productsController.delete.bind(this.productsController));
+    this.routes.patch("/disable/:id", auth, adminVerify, this.productsController.disable.bind(this.productsController));
+  }
+}
