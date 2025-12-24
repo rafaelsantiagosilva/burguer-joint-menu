4791c4aa91e7da21a13cd9c8993f2feb2a3cd978
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-23 22:20:18 -0300

    feat: register the products routes in the application

--- server/src/api/routers/index.ts
+++ server/src/api/routers/index.ts
@@ -1,10 +1,16 @@
-import type { Express } from "express";
-import { UserRouter } from "./user-router.ts";
-import { DrizzleUsersRepository } from "@/repositories/drizzle/drizzle-users-repository.ts";
 import { db } from "@/database/index.ts";
+import { DrizzleProductsRepository } from "@/repositories/drizzle/drizzle-products-repository.ts";
+import { DrizzleUsersRepository } from "@/repositories/drizzle/drizzle-users-repository.ts";
+import type { Express } from "express";
+import { ProductRouter } from "./product-router.ts";
+import { UserRouter } from "./user-router.ts";
 
 export function registerRouters(app: Express) {
   const usersRepository = new DrizzleUsersRepository(db);
   const userRouter = new UserRouter(usersRepository);
   app.use("/users", userRouter.routes);
+
+  const productsRepository = new DrizzleProductsRepository(db);
+  const productRouter = new ProductRouter(productsRepository);
+  app.use("/products", productRouter.routes);
 }
