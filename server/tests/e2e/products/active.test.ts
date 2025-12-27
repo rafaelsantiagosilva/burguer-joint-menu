57f7a06ddd83422128ec8c185df05c63f332c3b9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-27 16:29:28 -0300

    test: product e2e test -> PATCH /products/active/:id

---
+++ server/tests/e2e/products/active.test.ts
@@ -0,0 +1,78 @@
+import { app } from "@/app.ts";
+import { DrizzleProductsRepository } from "@/repositories/drizzle/drizzle-products-repository.ts";
+import { DrizzleUsersRepository } from "@/repositories/drizzle/drizzle-users-repository.ts";
+import { db, resetDatabase } from "@/tests/setup/db.ts";
+import { makeJwt } from "@/utils/make-jwt.ts";
+import { StatusCodes } from "http-status-codes";
+import request from "supertest";
+import { afterAll, beforeEach, describe, expect, it } from "vitest";
+
+describe("Product (E2E) - PATCH /products/active/:id", () => {
+  let productsRepository: DrizzleProductsRepository;
+  let usersRepository: DrizzleUsersRepository;
+
+  beforeEach(async () => {
+    await resetDatabase();
+    productsRepository = new DrizzleProductsRepository(db);
+    usersRepository = new DrizzleUsersRepository(db);
+
+  });
+
+  afterAll(async () => {
+    await resetDatabase();
+  });
+
+  it("should be able to active a product", async () => {
+    const user = await usersRepository.create("(01) X2345-6789", "123456", true);
+    const product = await productsRepository.create("Product 1", null, 10, null);
+    const token = makeJwt(user.id);
+
+    const response = await request(app).patch(`/products/active/${product.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(StatusCodes.NO_CONTENT);
+  });
+
+  it("should not be able to active a product that does not exist", async () => {
+    const user = await usersRepository.create("(01) X2345-6789", "123456", true);
+    const nonExistingProductId = crypto.randomUUID();
+    const token = makeJwt(user.id);
+
+    const response = await request(app)
+      .patch(`/products/active/${nonExistingProductId}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+  });
+
+  it("should not be able to active a product if the token is invalid", async () => {
+    const product = await productsRepository.create("Product 1", null, 10, null);
+    const token = "invalid-token";
+
+    const response = await request(app)
+      .patch(`/products/active/${product.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  it("should not be able to active a product if the token is missing", async () => {
+    const product = await productsRepository.create("Product 1", null, 10, null);
+
+    const response = await request(app)
+      .patch(`/products/active/${product.id}`)
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  it("should not be able to active a product if the user is not admin", async () => {
+    const user = await usersRepository.create("(01) X2345-6789", "123456", false);
+    const product = await productsRepository.create("Product 1", null, 10, null);
+    const token = makeJwt(user.id);
+
+    const response = await request(app).patch(`/products/active/${product.id}`)
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+});
