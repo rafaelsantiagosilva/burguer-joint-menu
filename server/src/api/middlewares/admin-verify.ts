e3c18ed828fec2cec2221d6a4cdf5f2e3e542f2e
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-27 17:00:27 -0300

    refactor: call the service instead the repository method

--- server/src/api/middlewares/admin-verify.ts
+++ server/src/api/middlewares/admin-verify.ts
@@ -1,12 +1,14 @@
 import { db } from "@/database/index.ts";
 import { DrizzleUsersRepository } from "@/repositories/drizzle/drizzle-users-repository.ts";
+import { GetUserProfileService } from "@/services/users/get-profile.ts";
 import type { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export async function adminVerify(req: Request, res: Response, next: NextFunction) {
   const { userId } = req;
   const usersRepository = new DrizzleUsersRepository(db);
-  const user = await usersRepository.findById(userId);
+  const getUserProfileService = new GetUserProfileService(usersRepository);
+  const user = await getUserProfileService.execute({ userId });
 
   if (!user || !user.isAdmin)
     return res.status(StatusCodes.UNAUTHORIZED).json({ message: "Unauthorized" });
