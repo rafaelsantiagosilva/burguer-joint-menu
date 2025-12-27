4d9f41e4a7fb103c5a94acc35ab4b0c4dffcc1f6
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-12-27 17:36:36 -0300

    refactor: changing the delete route url

--- server/src/api/routers/product-router.ts
+++ server/src/api/routers/product-router.ts
@@ -15,7 +15,7 @@ export class ProductRouter {
     this.routes.patch("/active/:id", auth, adminVerify, this.productsController.active.bind(this.productsController));
     this.routes.post("/create", auth, adminVerify, this.productsController.create.bind(this.productsController));
     this.routes.post("/upload/:id", auth, adminVerify, upload.single("image"), this.productsController.upload.bind(this.productsController));
-    this.routes.delete("/delete/:id", auth, adminVerify, this.productsController.delete.bind(this.productsController));
+    this.routes.delete("/:id", auth, adminVerify, this.productsController.delete.bind(this.productsController));
     this.routes.patch("/disable/:id", auth, adminVerify, this.productsController.disable.bind(this.productsController));
     this.routes.get("/list", auth, this.productsController.list.bind(this.productsController));
     this.routes.put("/update/:id", auth, adminVerify, this.productsController.update.bind(this.productsController));
