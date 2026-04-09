1d2d6bdcb63f8545213fafbab28864507c76e9be
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-04-08 21:46:29 -0300

    feat: enrich the hero section title

--- web/src/components/Hero/index.tsx
+++ web/src/components/Hero/index.tsx
@@ -3,7 +3,7 @@ import * as S from "./styles";
 export function Hero() {
     return (
         <S.Container>
-            <h1>Hero section</h1>
+            <h1>Hambúrgueres que fazem história, <br /> um bite de cada vez!</h1>
         </S.Container>
     )
 }
